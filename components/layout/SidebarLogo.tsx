514ca144518fa1d0fc95c70ad4867e8c76dce524
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-03-30 16:06:23 +0100

    second commit

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useRouter } from 'next/router'
+import {BsTwitter} from "react-icons/bs"
+
 
 const SidebarLogo = () => {
+ 
+    const router = useRouter()
+
   return (
-    <div>SidebarLogo</div>
+
+    <div className='rounded-full
+     h-14
+     w-14
+     p-4
+     flex
+     items-center
+     justify-center
+     hover:bg-blue-300
+     hover:bg-opacity-10
+     cursor-pointer
+    '>
+        <BsTwitter 
+        size={30} color="white "
+        />
+    </div>
   )
 }
 
