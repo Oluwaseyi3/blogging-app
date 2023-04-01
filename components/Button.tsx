b1fc93be040df758d40b50407e0803cba41385d1
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-04-01 14:49:56 +0100

    modals

---
+++ components/Button.tsx
@@ -0,0 +1,48 @@
+interface ButterProps {
+    label: string;
+    secondary?: boolean;
+    fullWidth?: boolean;
+    large?: Boolean;
+    disabled:boolean;
+    outline: boolean;
+    onClick: () => void;
+}
+
+
+const Button: React.FC<ButterProps> = ({
+    label,
+    secondary,
+    large,
+    onClick,
+    disabled,
+    outline,
+    fullWidth
+}) => {
+  return (
+  <button 
+  disabled={disabled}
+  onClick={onClick}
+  className={`disabled:opacity-70
+  disabled:cursor-not-allowed
+  rounded-full
+  font-semibold
+  hover:opacity-80
+  transition
+  border-2
+  ${fullWidth ? "w-full" : "w-fit"}
+  ${secondary ? "bg-white" : "bg-sky-500"}
+  ${secondary ? "text-black" : "text-white"}
+  ${secondary ? "border-black" : "border-sky-500"}
+  ${large ? "text-xl" : "text-md"}
+  ${large ? "px-5" : "px-4"}
+  ${large ? "py-3" : "py-2"}
+  ${outline ? "bg-transparent" : ""}
+  ${outline ? "border-white" : ""}
+  ${outline ? "text-white" : ""}
+  `}>
+  {label}
+  </button>
+  )
+}
+
+export default Button
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import Modal from '@/components/Modal'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+   
   <Layout>
      <Component {...pageProps} />
   </Layout>
+  </>
   )
 }
