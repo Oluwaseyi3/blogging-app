1df9e2bee54fb9d24489d03501d10f776e4aa059
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-04-04 16:07:02 +0100

    updated UserBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,6 +1,9 @@
 import useCurrentUser from '@/hooks/useCurrentUser';
 import useUser from '@/hooks/useUser';
-import React from 'react'
+import { format } from 'date-fns';
+import React,{useMemo} from 'react';
+import Button from '../Button';
+
 
 interface UserBioProps{
     userId: string;
@@ -11,8 +14,27 @@ const UserBio: React.FC<UserBioProps> = ({userId}) => {
    const {data: currentUser} = useCurrentUser()
   
    const  {data: fetchedUser} = useUser(userId)
+   console.log(currentUser);
+   console.log(userId);
+   
+   
+
+   const createdAt = useMemo(() => {
+      if (!fetchedUser?.createdAt) {
+        return null
+      }
+      return format(new Date(fetchedUser.createdAt), "MMMM yyyy")
+   }, [fetchedUser?.createdAt])
     return (
-    <div>UserBio</div>
+    <div className='border-b-[1px] border-neutral-800 pb-4  '>
+        <div className='flex justify-end p-2'>
+            {currentUser?.id === userId ? (
+                <Button secondary label="Edit" onClick={() => {}} />
+            ) : (
+                <Button onClick={() => {}} label="Follow" secondary />
+            )}
+        </div>
+    </div>
   )
 }
 
