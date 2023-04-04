37e09271be98ca227dd323343a8bb4a3b09316f1
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-04-04 15:40:56 +0100

    fixed UserBio

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,9 +1,37 @@
+import Header from "@/components/layout/Header"
+import useUser from "@/hooks/useUser"
+import { useRouter } from "next/router"
+import { ClipLoader } from "react-spinners"
+import UserHero from "@/components/users/UserHero"
+import UserBio from "@/components/users/UserBio"
 
- 
  const UserView = () => {
+
+    const router = useRouter()
+   const { userId} = router.query
+
+   const {data: fetchedUser, isLoading} = useUser(userId as string)
+
+   if(isLoading || !fetchedUser){
+      return(
+        <div className="
+         flex
+         justify-center
+         items-center
+         h-full 
+        ">
+            <ClipLoader color="red" size={90} />
+        </div>
+      )
+   }
+
    return (
-     <div></div>
-   )
+    <>
+    <Header showBackArrow label={fetchedUser?.name} />
+    <UserHero userId={userId as string} />
+    <UserBio userId={userId as string} />
+    </>
+     )
  }
  
  export default UserView
