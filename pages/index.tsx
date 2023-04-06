f45cbfe6f88a8c6b30cd66394a6e6d7f6784ff56
Oluwaseyi3 <iamoluwaseyiolasupo@gmail.com>
2023-04-06 11:40:58 +0100

    added posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@ import { NextPage } from "next"
 import Head from "next/head"
 import Header from "@/components/layout/Header"
 import Form from "@/components/Form"
+import PostFeed from "@/components/posts/PostFeed"
 
 export default function Home() {
   return (
     <>
       <Header  label="Home"/> 
-      <Form placeholder="What's happening" />
+      <Form placeholder="Share your thoughts" />
+      <PostFeed/>
     </>
   )
 }
